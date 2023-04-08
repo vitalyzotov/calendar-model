dfa07c6f5087708de4a880443b393cac8d20e603
vzotov <vitalyzotov@gmail.com>
2023-04-08 07:38:03 +0400

    Search for intersections for date intervals

--- src/main/java/ru/vzotov/calendar/domain/model/DateRange.java
+++ src/main/java/ru/vzotov/calendar/domain/model/DateRange.java
@@ -13,6 +13,12 @@ public class DateRange<T extends Temporal> implements ValueObject<DateRange<T>> 
     private final T start;
     private final T finish;
 
+    /**
+     * Creates a new date range between start (inclusive) and finish (inclusive)
+     *
+     * @param start  start of the date range (inclusive)
+     * @param finish finish of the date range (inclusive)
+     */
     public DateRange(T start, T finish) {
         Validate.notNull(start);
         Validate.notNull(finish);
@@ -25,38 +31,96 @@ public class DateRange<T extends Temporal> implements ValueObject<DateRange<T>> 
         this.finish = finish;
     }
 
+    /**
+     * Creates a new date range from a month
+     *
+     * @param yearMonth the month
+     * @return a new date range between the first day of month and the last day of month
+     */
     public static DateRange<LocalDate> of(YearMonth yearMonth) {
         return new DateRange<>(yearMonth.atDay(1), yearMonth.atEndOfMonth());
     }
 
+    /**
+     * Creates a new date range between start (inclusive) and finish (inclusive)
+     *
+     * @param start  start of the date range (inclusive)
+     * @param finish finish of the date range (inclusive)
+     */
     public static <T extends Temporal> DateRange<T> of(T start, T finish) {
         return new DateRange<>(start, finish);
     }
 
+    /**
+     * Gets the start of the date range
+     *
+     * @return the value of start of the date range
+     */
     public T start() {
         return start;
     }
 
+    /**
+     * Gets the finish of the date range
+     *
+     * @return the value of finish of the date range
+     */
     public T finish() {
         return finish;
     }
 
+    /**
+     * Calculates the amount of time between start and finish of the date range
+     *
+     * @param unit the unit
+     * @return the amount of time between start and finish
+     */
     public long width(ChronoUnit unit) {
         return unit.between(start, finish);
     }
 
+    /**
+     * Creates a new date range that starts at specified time and finishes at the same time as this date range
+     *
+     * @param start the new start time
+     * @return new date range
+     */
     public DateRange<T> startingAt(T start) {
         return new DateRange<>(start, this.finish);
     }
 
+    /**
+     * Creates a new date range that starts at the same time as this date range and finishes at specified time
+     *
+     * @param finish the new finish time
+     * @return the new date range
+     */
     public DateRange<T> finishingAt(T finish) {
         return new DateRange<>(this.start, finish);
     }
 
+    /**
+     * Checks that the date range contains the specified date
+     *
+     * @param date the date to check
+     * @return true if the date range includes the specified date; false otherwise
+     */
     public boolean includes(LocalDate date) {
-        return ChronoUnit.DAYS.between(start, date) >= 0 && ChronoUnit.DAYS.between(date, finish) >= 0;
+        return includes(date, ChronoUnit.DAYS);
     }
 
+    public boolean includes(Temporal temporal, ChronoUnit unit) {
+        return unit.between(start, temporal) >= 0 && unit.between(temporal, finish) >= 0;
+    }
+
+    /**
+     * Checks that the date range is completely included into another date range.
+     *
+     * @param other another date range to check
+     * @param unit  the unit that will be used to compare two date ranges
+     * @return this date range if the date range is completely included into other date range;
+     * otherwise returns the intersection of the two date ranges
+     */
     public DateRange<T> within(DateRange<T> other, ChronoUnit unit) {
         Validate.notNull(other);
         boolean invalid = false;
@@ -71,6 +135,24 @@ public class DateRange<T extends Temporal> implements ValueObject<DateRange<T>> 
             f = other.finish;
         }
         return invalid ? new DateRange<>(s, f) : this;
+    }
+
+    /**
+     * Overlaps the date range with other date range
+     * @param other the other date range
+     * @param unit the unit
+     * @return intersection of two ranges; null if ranges do not overlap.
+     */
+    public DateRange<T> intersect(DateRange<T> other, ChronoUnit unit) {
+        Validate.notNull(other);
+        T s1 = this.start;
+        T f1 = this.finish;
+        T s2 = other.start;
+        T f2 = other.finish;
+        boolean intersects = this.includes(s2, unit) || this.includes(f2, unit)
+                || other.includes(s1, unit) || other.includes(f1, unit);
+
+        return intersects ? new DateRange<>(max(s1, s2, unit), min(f1, f2, unit)) : null;
     }
 
     @Override
@@ -95,5 +177,13 @@ public class DateRange<T extends Temporal> implements ValueObject<DateRange<T>> 
     public boolean sameValueAs(DateRange that) {
         return that != null && Objects.equals(start, that.start) &&
                 Objects.equals(finish, that.finish);
+    }
+
+    private static <T extends Temporal> T max(T t1, T t2, ChronoUnit unit) {
+        return unit.between(t1, t2) > 0 ? t2 : t1;
+    }
+
+    private static <T extends Temporal> T min(T t1, T t2, ChronoUnit unit) {
+        return unit.between(t1, t2) < 0 ? t2 : t1;
     }
 }
--- src/test/java/ru/vzotov/calendar/domain/model/DateRangeTest.java
+++ src/test/java/ru/vzotov/calendar/domain/model/DateRangeTest.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(JUnit4.class)
@@ -23,5 +27,26 @@ public class DateRangeTest {
                 LocalDate.of(2021, 1, 10),
                 LocalDate.of(2021, 4, 25)
         ));
+    }
+
+    @Test
+    public void testIntersect() {
+        assertThat(
+                DateRange.of(LocalDate.parse("2022-01-01"), LocalDate.parse("2022-01-31"))
+                        .intersect(
+                                DateRange.of(LocalDate.parse("2022-01-20"), LocalDate.parse("2022-03-31")),
+                                ChronoUnit.DAYS
+                        )
+        ).isEqualTo(
+                DateRange.of(LocalDate.parse("2022-01-20"), LocalDate.parse("2022-01-31"))
+        );
+
+        assertThat(
+                DateRange.of(LocalDate.parse("2022-01-01"), LocalDate.parse("2022-01-31"))
+                        .intersect(
+                                DateRange.of(LocalDate.parse("2022-02-01"), LocalDate.parse("2022-02-28")),
+                                ChronoUnit.DAYS
+                        )
+        ).isNull();
     }
 }
